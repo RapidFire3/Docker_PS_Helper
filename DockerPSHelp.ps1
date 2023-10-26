7ee565dd75c827022003fccd34cd9867e47dd39d
Jeff Gardner <46628796+RapidFire3@users.noreply.github.com>
2023-10-26 11:57:55 -0400

    Add files via upload
    Added a help section to the two functions and also updated the some comments.

--- DockerPSHelp.ps1
+++ DockerPSHelp.ps1
@@ -1,17 +1,12 @@
 ###########################################################################
 #   Docker PowerShell Help Script
 #   Author: Jeff Gardner
-#
-#   Used only for windows
 ###########################################################################
-
-
-
-
+ 
 ###########################################################################
-#   If the user would like to load in a configuration file.
+#   If the user would like to load in a configuration file for paths.
 ###########################################################################
-#[xml]$configFile
+#[xml]$configFile - Get-Contents <CONFIG_FILE>
 
 
 ###########################################################################
@@ -29,6 +24,17 @@ enum ContainerEngineOptions {
 ###########################################################################
 function Set-DockerRegistry
 {
+    <#
+        .SYNOPSIS
+        Logging into a desired Docker registry.
+
+        .DESCRIPTION
+        Responsible for logging into the desired Docker registry.
+
+        .NOTES
+        Author: Jeff Gardner
+    #>
+
     [string]$private:dockerRegistry = "<DESIRED_REGISTRY>"
 
     ###########################################################################
@@ -36,7 +42,7 @@ function Set-DockerRegistry
     #
     #   String can be modified at the developer's discretion.
     ###########################################################################
-    Write-Host "`n `t>>> Please enter in your credentials <<< `n" -ForegroundColor Green
+    Write-Host "`n `t >>> Please enter in your credentials <<< `n" -ForegroundColor Green
 
     ###########################################################################
     #   Responsible for launching the web page where the user's API Key is 
@@ -44,7 +50,8 @@ function Set-DockerRegistry
     #   having to search for the search, resullting in a reduction of process 
     #   time. This is for projects that maybe using enterprise tools that 
     #   Utilizes API keys. Maybe not be needed if the user is just using an 
-    #   individual account such as from dockerhub.
+    #   individual account such as from dockerhub. Could be set using the 
+    #   the configuration file.
     ###########################################################################
     #Start-Process "<WEB-PAGE>"
     
@@ -59,15 +66,39 @@ function Set-DockerRegistry
 #   running for the desired container operating system. If not, this 
 #   function will switch to desired container.
 ###########################################################################
-function Confirm-DockerWindowsContainers
+function Confirm-DockerEngine
 {
+    <#
+        .SYNOPSIS
+        Confirms that Docker is configured using the desired Docker daemon
+        engine.
+
+        .DESCRIPTION
+        Responsible for confirming that Docker is configured using the desired 
+        Docker daemon engine. This can either be configured either using linux
+        or windows containers. This command can only be executed on a windows
+        host.
+
+        .PARAMETER DockerEngineOS  
+        The desired operating system to run the Docker daemon engine in. This 
+        could be linux or windows. 
+
+        .NOTES
+        Author: Jeff Gardner
+    #>
+
     [CmdletBinding()]
     param(
         [ContainerEngineOptions]$DockerEngineOS = [ContainerEngineOptions]::linux
     )
 
-    [string]$private:dockerApp          =   "C:\'Program Files'\Docker\Docker\resources\bin\docker.exe"
-    [string]$private:dockerCLIApp       =   "C:\'Program Files'\Docker\Docker\dockercli.exe"
+    ###########################################################################
+    #   Paths for the Docker neccessary components. These are the default
+    #   paths, but may vary. Tune accordinly. Could be set with a configuration
+    #   file.
+    ###########################################################################
+    [string]$private:dockerApp      =   "C:\'Program Files'\Docker\Docker\resources\bin\docker.exe"
+    [string]$private:dockerCLIApp   =   "C:\'Program Files'\Docker\Docker\dockercli.exe"
 
     ###########################################################################
     #   Responsible for getting the current operating system that the Docker 
@@ -80,9 +111,9 @@ function Confirm-DockerWindowsContainers
     #   Check the path for the Docker application and the Docker application
     #   exist. These two are needed to conduct the following operations.
     #
-    #   I removed the the single quotes from the string because the "path" does 
+    #   Removed the the single quotes from the string because the "path" does 
     #   not actually contains single quotes.
-
+    #
     #   Added the single qupotes to escape the space if there are any.
     ###########################################################################   
     if( ( Test-Path -Path ( $dockerApp -replace "'", "" )) -and 
@@ -117,14 +148,14 @@ function Confirm-DockerWindowsContainers
         #   Display status.
         ###########################################################################
         else {
-            Write-Host "`tAlready using $DockerEngineOS Containers engine .`n" -ForegroundColor Green
+            Write-Host "`t Already using $DockerEngineOS Containers engine .`n" -ForegroundColor Green
         }
     }
 
     ###########################################################################
-    #   If the Docker CLI path could not be located on the machine.
+    #   Display status.
     ###########################################################################
     else {
-        Write-Host "`tCould not locate the Docker CLI application.`n" -ForegroundColor Red
+        Write-Host "`t Could not locate the neccessary Docker applications.`n" -ForegroundColor Red
     }
 }
